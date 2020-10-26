5085e23f63f618c7f7487b2c0a62fb7ecc2119b5
Christoffer Kronblad <crisse.kronblad@gmail.com>
2020-10-26 20:02:06 +0100

    Final commit

--- Backpack/src/code.ts
+++ Backpack/src/code.ts
@@ -1,5 +1,7 @@
 let itemsText = document.querySelector(".items");
 let table = document.querySelector(".table");
+let tableBag = document.querySelector('.tableBag');
+
 let items: Item[] = [];
 
 
@@ -23,66 +25,158 @@ class Backpack {
     maxVolume: Number;
     itemArray: Item[];
     insideBag: Item[];
-
+    amount: number[];
+    totalWeight: number[];
+    totalVolume: number[];
+    allWeight: number;
+    allVolume: number;
 
     constructor(items: Item[]) {
         this.maxWeight = 60;
         this.maxVolume = 50;
         this.itemArray = items;
         this.insideBag = [];
+        this.amount = [];
+        this.totalWeight = [];
+        this.totalVolume = [];
+        this.allWeight = 0;
+        this.allVolume = 0;
+
+        for (let i = 0; i < items.length; i++) {
+            this.amount.push(0);
+            this.totalWeight.push(0);
+            this.totalVolume.push(0);
+        }
     }
 
 
     AddItem(articleID: number) {
 
+        if (this.allWeight < this.maxWeight
+            && this.allVolume < this.maxVolume
+            && this.itemArray[articleID].weight + this.allWeight < this.maxWeight
+            && this.itemArray[articleID].weight + this.allVolume < this.maxVolume) {
 
 
+            if (this.amount[articleID] < 1) {
+                this.insideBag.push(this.itemArray[articleID]);
+            }
+            this.amount[articleID]++;
+            this.totalWeight[articleID] = this.itemArray[articleID].weight * this.amount[articleID];
+            this.totalVolume[articleID] = this.itemArray[articleID].volume * this.amount[articleID];
 
-        this.insideBag.push(this.itemArray[articleID]);
-        console.log(this.insideBag)
+            this.CurrentWeight();
+            this.CurrentVolume();
+
+            this.ShowItems()
+
+        }
     }
 
-    RemoveItem(articleID: number, number: number) { }
+    RemoveItem(articleID: number, number: number) {
+
+
+        if (this.amount[articleID] > 0) {
+
+            this.amount[articleID]--;
+            this.totalWeight[articleID] = this.itemArray[articleID].weight * this.amount[articleID];
+            this.totalVolume[articleID] = this.itemArray[articleID].volume * this.amount[articleID];
+            if (this.amount[articleID] < 1) {
+                this.insideBag = this.insideBag.filter(item => item.id != articleID);
+            }
+
+            this.CurrentWeight();
+            this.CurrentVolume();
+
+            this.ShowItems()
+        }
+    }
 
     ShowItems() {
 
+        tableBag!.innerHTML =
+            `
+        <tr>
+        <th>Artikel</th>
+        <th>Antal</th>
+        <th>Vikt/st (Kg)</th>
+        <th>Volym/st (liter)</th>
+        <th>Total Vikt (Kg)</th>
+        <th>Total Volym (liter)</th>
+        </tr>
+        `;
+        table!.innerHTML =
+            `
+        <tr>
+        <th>Artikel</th>
+        <th>Antal</th>
+        <th>Vikt/st (Kg)</th>
+        <th>Volym/st (liter)</th>
+        <th>Total Vikt (Kg)</th>
+        <th>Total Volym (liter)</th>
+        </tr>
+        `;
         for (let item of this.itemArray) {
 
             table!.innerHTML +=
                 `
             <tr>
             <td>${item.description}</td>
-            <td><button id="${item.description}Min">-</button> 0 <button id="${item.description}Plus">+</button></td>
+            <td><button id="${item.description}Min">-</button> ${this.amount[item.id]} <button id="${item.description}Plus">+</button></td>
             <td>${item.weight}</td>
             <td>${item.volume}</td>
-            <td>max vikt</td>
-            <td>max volym</td>
+            <td>${this.totalWeight[item.id].toFixed(2)}</td>
+            <td>${this.totalVolume[item.id].toFixed(2)}</td>
             </tr>
             `;
+
+        }
+        for (let item of this.itemArray) {
             let btnMinus = document.querySelector(`#${item.description}Min`)
-            // console.log(btnMinus);
             btnMinus!.addEventListener('click', () => this.RemoveItem(item.id, 1));
             let btnPlus = document.querySelector(`#${item.description}Plus`)
             btnPlus!.addEventListener('click', () => this.AddItem(item.id));
-            console.log(btnPlus)
-
         }
+        if (this.insideBag.length > 0) {
+            for (let item of this.insideBag) {
+                tableBag!.innerHTML +=
+                    `
+                <tr>
+                <td>${item.description}</td>
+                <td><button id="${item.description}MinBag">-</button> ${this.amount[item.id]} <button id="${item.description}PlusBag">+</button></td>
+                <td>${item.weight}</td>
+                <td>${item.volume}</td>
+                <td>${this.totalWeight[item.id].toFixed(2)}</td>
+                <td>${this.totalVolume[item.id].toFixed(2)}</td>
+                </tr>
+                `;
+            }
+            for (let item of this.insideBag) {
+                let btnMinusBag = document.querySelector(`#${item.description}MinBag`)
+                btnMinusBag!.addEventListener('click', () => this.RemoveItem(item.id, 1));
+                let btnPlusBag = document.querySelector(`#${item.description}PlusBag`)
+                btnPlusBag!.addEventListener('click', () => this.AddItem(item.id));
+            }
+        }
+
     }
 
     CurrentWeight() {
 
-
+        this.allWeight = 0;
+        for (let i = 0; i < this.itemArray.length; i++) {
+            this.allWeight += this.totalWeight[i];
+        }
+        console.log("Weight: " + this.allWeight)
     }
 
     CurrentVolume() {
-
-
+        this.allVolume = 0;
+        for (let i = 0; i < this.itemArray.length; i++) {
+            this.allVolume += this.totalVolume[i];
+        }
+        console.log("Volume: " + this.allVolume)
     }
-
-    async fetchItems() {
-
-    }
-
 }
 
 async function fetchItems() {
