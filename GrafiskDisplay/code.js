7b70ee515f335116f746de032f4b4356983a1c01
Christoffer Kronblad <crisse.kronblad@gmail.com>
2020-11-12 15:10:56 +0100

    Added fadeIn and FadeOut

--- GrafiskDisplay/code.js
+++ GrafiskDisplay/code.js
@@ -20,18 +20,18 @@ class Canvas {
         for (let i = 0; i < this.amountOfRec * this.amountOfRec; i++) {
 
 
-            if (black) {
+            // if (black) {
 
-                this.draw.fillStyle = `#000000`;
-            } else {
-                this.draw.fillStyle = `#FFFFFF`;
+            //     this.draw.fillStyle = `#000000`;
+            // } else {
+            //     this.draw.fillStyle = `#FFFFFF`;
 
-            }
+            // }
 
             let r = Math.floor(Math.random() * 256);
             let g = Math.floor(Math.random() * 256);
             let b = Math.floor(Math.random() * 256);
-            // this.draw.fillStyle = `rgb(${r},${g} ,${b})`;
+            this.draw.fillStyle = `rgb(${r},${g},${b})`;
 
             // this.draw.fillStyle = `#704214`;
 
@@ -39,12 +39,12 @@ class Canvas {
             if (i % this.amountOfRec == 0 && i != 0) {
                 this.xPos = 0;
                 this.yPos++;
-                black = !black;
             }
             this.draw.fillRect((canvas.width / this.amountOfRec) * this.xPos, (canvas.width / this.amountOfRec) * this.yPos, (canvas.width / this.amountOfRec), (canvas.height / this.amountOfRec));
             this.recArr.push([(canvas.width / this.amountOfRec) * this.xPos, (canvas.width / this.amountOfRec) * this.yPos, (canvas.width / this.amountOfRec), (canvas.height / this.amountOfRec)]);
 
             this.xPos++;
+            black = !black;
         }
     }
 
@@ -60,32 +60,75 @@ class Canvas {
 
 
     fadeIn() {
+
         let target = 255;
-        this.draw.clearRect(0, 0, canvas.width, canvas.height)
-        for (let i = 0; i < this.recArr.length; i++) {
+        let index = 0;
 
-            let startRed = parseInt(this.colorArr[i][1] * this.colorArr[i][2] - 1);
-            let startGreen = parseInt(this.colorArr[i][3] * this.colorArr[i][4] - 1)
-            let startBlue = parseInt(this.colorArr[i][6] * this.colorArr[i][7] - 1);
 
-            let red = (target - parseInt(this.colorArr[i][1] * this.colorArr[i][2] - 1)) / this.recArr.length;
-            let green = (target - parseInt(this.colorArr[i][3] * this.colorArr[i][4] - 1)) / this.recArr.length;
-            let blue = (target - parseInt(this.colorArr[i][6] * this.colorArr[i][7] - 1)) / this.recArr.length;
+        let interval = setInterval(() => {
 
-            this.draw.fillStyle = `rgb(${startRed += red},${startGreen += green},${startBlue += blue})`;
-            this.draw.fillRect(this.recArr[i][0], this.recArr[i][1], this.recArr[i][2], this.recArr[i][3])
-        }
-        // setInterval(function () {
-        //     clearInterval(interval);
-        // }, 25);
+            if (index >= 16) {
 
+                clearInterval(interval);
+                return;
+            }
+
+            for (let i = 0; i < this.recArr.length; i++) {
+
+                let r = parseInt(this.colorArr[i][1] + this.colorArr[i][2], 16);
+                let g = parseInt(this.colorArr[i][3] + this.colorArr[i][4], 16);
+                let b = parseInt(this.colorArr[i][5] + this.colorArr[i][6], 16);
+
+                let newR = ((target - r) / 16) * (index + 1) + r;
+                let newG = ((target - g) / 16) * (index + 1) + g;
+                let newB = ((target - b) / 16) * (index + 1) + b;
+
+
+                this.draw.fillStyle = `rgb(${newR},${newG},${newB})`
+                this.draw.fillRect(this.recArr[i][0], this.recArr[i][1], this.recArr[i][2], this.recArr[i][3])
+            }
+
+            console.log(this.draw.fillStyle)
+            index++;
+
+        }, 100)
     }
 
 
-    fadeOut() {}
+    fadeOut() {
+        let index = 0;
+
+
+        let interval = setInterval(() => {
+
+            if (index > 16) {
+
+                clearInterval(interval);
+                return;
+            }
+
+            for (let i = 0; i < this.recArr.length; i++) {
+
+                let r = parseInt(this.colorArr[i][1] + this.colorArr[i][2], 16);
+                let g = parseInt(this.colorArr[i][3] + this.colorArr[i][4], 16);
+                let b = parseInt(this.colorArr[i][5] + this.colorArr[i][6], 16);
+
+                let newR = r - ((r / 16) * index + 1);
+                let newG = g - ((g / 16) * index + 1);
+                let newB = b - ((b / 16) * index + 1);
+
+
+                this.draw.fillStyle = `rgb(${newR},${newG},${newB})`
+                this.draw.fillRect(this.recArr[i][0], this.recArr[i][1], this.recArr[i][2], this.recArr[i][3])
+            }
+
+            console.log(this.draw.fillStyle)
+            index++;
+
+        }, 100)
+    }
 
     scrollLeft() {
-        this.draw.clearRect(0, 0, canvas.width / this.amountOfRec, canvas.height)
 
 
         for (let i = 0; i < this.recArr.length; i++) {
@@ -94,11 +137,11 @@ class Canvas {
             this.draw.fillRect(this.recArr[i][0], this.recArr[i][1], this.recArr[i][2], this.recArr[i][3])
         }
 
+        this.draw.clearRect(0, 0, canvas.width / this.amountOfRec, canvas.height)
         this.draw.clearRect(canvas.width - canvas.width / this.amountOfRec, 0, canvas.width / this.amountOfRec, canvas.height)
     }
 
     scrollRight() {
-        this.draw.clearRect(canvas.width - canvas.width / this.amountOfRec, 0, canvas.width / this.amountOfRec, canvas.height)
 
         for (let i = 0; i < this.recArr.length; i++) {
 
@@ -106,6 +149,7 @@ class Canvas {
             this.draw.fillRect(this.recArr[i][0], this.recArr[i][1], this.recArr[i][2], this.recArr[i][3])
         }
 
+        this.draw.clearRect(canvas.width - (canvas.width / this.amountOfRec), 0, canvas.width / this.amountOfRec, canvas.height)
         this.draw.clearRect(0, 0, canvas.width / this.amountOfRec, canvas.height)
     }
 
@@ -145,7 +189,6 @@ class Canvas {
     }
 
     circle(x, y, radius, color) {
-
         this.putPixle(x, y, color)
         for (let i = 1; i < 360; i++) {
             this.putPixle(Math.round(Math.cos(i) * radius) + x, Math.round(Math.sin(i) * radius) + y, color)
@@ -160,10 +203,10 @@ let data = new Canvas();
 // data.clear("#ff0000");
 // data.putPixle(22, 10, "#00FF00");
 // data.line(0, 0, 20, 35, "#000000");
-// data.circle(20, 20, 20, "#000000");
+// data.circle(20, 20, 10, "#000000");
 
 // Inte klara //
 // data.fadeIn();
-// data.fadeOut();
+data.fadeOut();
 // data.scrollLeft();
-data.scrollRight();
+// data.scrollRight();
